44aef0f6aaeaca2e4b40f7af552f158492758501
Antoine <116259241+antoinesoreau@users.noreply.github.com>
2025-12-16 19:16:29 +0100

    Refactor database connection in database.php
    Refactor database connection handling and update error messages.

--- config/database.php
+++ config/database.php
@@ -1,32 +1,14 @@
 <?php
+$username = '';
+$pass = '';
+$host = '';
+$name = 'jpo_mmi';
 
-class Database
-{
 
-    private static $instance = null;
-    private $pdo;
-
-    private function __construct()
-    {
-        $host = '127.0.0.1';
-        $dbname = 'jsp c quoi';
-        $user = 'non plus';
-        $password = 'non plus.';
-        $dsn = "mysql:host=$host;dbname=$dbname;charset=UTF8";
-
-        try {
-            $this->pdo = new PDO($dsn, $user, $password);
-            $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        } catch (PDOException $e) {
-            die("Erreur de connexion : " . $e->getMessage());
-        }
-    }
-
-    public static function getInstance()
-    {
-        if (self::$instance === null) {
-            self::$instance = new Database();
-        }
-        return self::$instance->pdo;
-    }
+try {
+    $dtb = new PDO('mysql:host='.$host.':3306;dbname='.$name.';charset=utf8mb4', $username, $pass);
+} catch (exception $e) {
+    die('Erreur : ' . $e->getMessage());
 }
+
+// Aide, pour appeler la bdd dans une fonction, faites jutse global $dtb;
